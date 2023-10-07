38a6acb53fcd36d7aca5ac468a8656c304af36f0
Konstantin <k.podyganov@mail.ru>
2023-10-07 21:40:52 +0300

    Added dependencies and backend database interaction

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
+
+    let data = sqlx::query!("");
 }
